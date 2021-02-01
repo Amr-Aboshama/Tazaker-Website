d73a8b1ef1a4501a64f00a05539b514c31c4d3ab
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-02-01 20:04:25 +0200

    Modifying Routes

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('auth')->middleware('jwt:api')->group(function () {
+    
+});
+
+Route::prefix('unauth')->group(function () {
+    Route::post('/signUp', [AuthenticationController::class, 'signUp']);
+    Route::post('/signIn', [AuthenticationController::class, 'signIn']);
+
+});
+
+Route::prefix('admin')->middleware('jwt:api')->group(function() {
+
 });
