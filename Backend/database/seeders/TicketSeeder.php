a4d23371b403a1d79f727635fec4df25e07f33fe
Amr-Aboshama <31421825+Amr-Aboshama@users.noreply.github.com>
2021-02-03 17:33:59 +0200

    Seeders: adding more data

--- Backend/database/seeders/TicketSeeder.php
+++ Backend/database/seeders/TicketSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TicketSeeder extends Seeder
 {
@@ -13,6 +14,20 @@ class TicketSeeder extends Seeder
      */
     public static function run()
     {
-        //
+        DB::table('tickets')
+            ->insert([
+                'username' => 'amr',
+                'match_id' => 1,
+                'seat_row' => 3,
+                'seat_column' => 3,
+            ]);
+
+        DB::table('tickets')
+            ->insert([
+                'username' => 'amr',
+                'match_id' => 1,
+                'seat_row' => 3,
+                'seat_column' => 2,
+            ]);
     }
 }
